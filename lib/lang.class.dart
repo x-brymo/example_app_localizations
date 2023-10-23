f7fa2fe6418ebb93ab5cdc86cebe79dae58f210d
Mahmoud Hafez Eltarqi <98850036+x-brymo@users.noreply.github.com>
2023-10-23 22:59:55 +0300

    easy_localizations_try

---
+++ lib/lang.class.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Translations {
+  static final all = [
+    const Locale('en'),
+    const Locale('ar'),
+  ];
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,24 @@
-import 'package:example_app_localizations/lang/app_locatizations.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final appLocalization = AppLocalizations(locale: const Locale('en'));
-  await appLocalization.loadJsonLanguage();
-  runApp(const MainApp());
-}
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final translatedHello = "hello".tr(context);
-    final translatedName = "name".tr(context);
     return MaterialApp(
-      locale: const Locale('en', 'US'),
-      supportedLocales: const [Locale('en'), Locale('ar')],
-      localizationsDelegates: const [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate
-      ],
-      localeResolutionCallback: (deviceLocale, supportedLocales) {
-        for (var locale in supportedLocales) {
-          if (deviceLocale != null &&
-              deviceLocale.languageCode == locale.languageCode) {
-            return deviceLocale;
-          }
-        }
-
-        return supportedLocales.first;
-      },
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      // localeResolutionCallback: (deviceLocale, supportedLocales) {
+      //   for (var locale in supportedLocales) {
+      //     if (deviceLocale != null &&
+      //         deviceLocale.languageCode == locale.languageCode) {
+      //       return deviceLocale;
+      //     }
+      //   }supportedLocales.first;
+      // },
       home: Scaffold(
         appBar: AppBar(
           title: const Text("Main App"),
@@ -44,13 +27,13 @@ class MainApp extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Center(
-              child: Text(AppLocalizations.of(context).translate("hello")),
+              child: Text("hello".tr()),
             ),
             Center(
-              child: Text(AppLocalizations.of(context).translate("hello")),
+              child: Text("hello".tr()),
             ),
             Center(
-              child: Text(AppLocalizations.of(context).translate("hello")),
+              child: Text("hello".tr()),
             ),
           ],
         ),
